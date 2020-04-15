006dc163721dfbb527d1bb05b1425608fb4914f9
shakib77 <shakibshaon77@gmail.com>
2020-04-15 09:59:51 +0600

    Updates

---
+++ precache-manifest.d9d2c886f6f3be322dc202ca83dad784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2209483f8a83eec0c9524bb63a1070e8",
+    "url": "/monster-rolodex-reactJS/index.html"
+  },
+  {
+    "revision": "a8a30861578233f5cabd",
+    "url": "/monster-rolodex-reactJS/static/css/main.45072210.chunk.css"
+  },
+  {
+    "revision": "a4a9a92535ed4793cbee",
+    "url": "/monster-rolodex-reactJS/static/js/2.9320a8c0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex-reactJS/static/js/2.9320a8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a30861578233f5cabd",
+    "url": "/monster-rolodex-reactJS/static/js/main.fda21195.chunk.js"
+  },
+  {
+    "revision": "3740f1942f130a56f58b",
+    "url": "/monster-rolodex-reactJS/static/js/runtime-main.373902ae.js"
+  }
+]);
